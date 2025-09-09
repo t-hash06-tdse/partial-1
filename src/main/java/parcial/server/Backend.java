057fd2c52c2fcca54de44b5003bfca740167ee58
t-hash06 <tomasfelipe.p@gmail.com>
2025-09-09 10:05:37 -0500

    feat: add basic key-value database and handlers

--- src/main/java/parcial/server/Backend.java
+++ src/main/java/parcial/server/Backend.java
@@ -1,13 +1,135 @@
 package parcial.server;
 
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.*;
 
 public class Backend {
     private final int PORT;
+    private final HashMap<String, String> database;
 
-    public Backend(int port){
+    public Backend(int port) {
         this.PORT = port;
+        this.database = new HashMap<>();
+    }
+
+    private Map<String, String> getParams(String path) {
+        HashMap<String, String> result = new HashMap<>();
+
+        if (path == null) {
+            return result;
+        }
+
+        String[] parts = path.split("\\?");
+
+        if (parts.length != 2) {
+            return result;
+        }
+
+        String params = parts[1];
+        String[] pairs = params.split("&");
+
+        for (String pair : pairs) {
+            String[] pairParts = pair.split("=");
+
+            if (pairParts.length != 2) {
+                continue;
+            }
+
+            result.put(pairParts[0], pairParts[1]);
+        }
+
+        return result;
+    }
+
+    private Map<String, String> getRequestLine(String request) {
+
+        HashMap<String, String> result = new HashMap<>();
+        String firstLine = request.split("\n")[0];
+        String[] parts = firstLine.split(" ");
+
+        if (parts.length != 3) {
+            System.out.println("Request line invalido: " + firstLine);
+        }
+
+        result.put("method", parts[0]);
+        result.put("path", parts[1]);
+        result.put("protocol", parts[2]);
+
+        return result;
+    }
+
+    private String getBasicResponse(String value) {
+        String outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Title of the document</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>" + value + "</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+
+        return outputLine;
+    }
+
+    private void handleGetKeyValue(PrintWriter output, Map<String, String> params) {
+        String key = params.get("key");
+
+        if (key == null) {
+            String outputLine = getBasicResponse("No se proporciono el param \"key\"");
+            output.println(outputLine);
+            return;
+        }
+
+        String value = database.get(key);
+
+        if (value == null) {
+            String outputLine = getBasicResponse("No existe la llave \"" + key + "\"");
+            output.println(outputLine);
+            return;
+        }
+
+        String outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n"
+                + "[ \"key\": \"" + key + "\", \"value\": \"" + value + "\"]";
+        output.println(outputLine);
+    }
+
+    private void handleSetkeyValue(PrintWriter output, Map<String, String> params) {
+        String key = params.get("key");
+
+        if (key == null) {
+            String outputLine = getBasicResponse("No se proporciono el param \"key\"");
+            output.println(outputLine);
+            return;
+        }
+
+        String value = params.get("value");
+
+        if (value == null) {
+            String outputLine = getBasicResponse("No se proporciono el param \"value\"");
+            output.println(outputLine);
+            return;
+        }
+
+        database.put(key, value);
+        String outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n"
+                + "[ \"key\": \"" + key + "\", \"value\": \"" + value + "\", \"status\": \"created\"]";
+        output.println(outputLine);
+    }
+
+    public void handleDefaultRoute(PrintWriter output) {
+        output.println(getBasicResponse(
+                "No ingresaste una ruta valida entre /setkv?key=<key>&value=<value> o /getkv?key=<key>"));
     }
 
     public void start() throws IOException {
@@ -19,42 +141,47 @@ public class Backend {
             System.exit(1);
         }
 
+        System.out.println("Servidor corriendo en http://localhost:" + this.PORT);
         Socket clientSocket = null;
-        try {
-            System.out.println("Listo para recibir ...");
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-        PrintWriter out = new PrintWriter(
-                clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Recibí: " + inputLine);
-            if (!in.ready()) {
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
                 break;
             }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            String request = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                request += inputLine + "\n";
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            Map<String, String> requestLineParts = getRequestLine(request);
+            String path = requestLineParts.get("path");
+            Map<String, String> params = getParams(path);
+
+            if (path.startsWith("/getkv")) {
+                handleGetKeyValue(out, params);
+            } else if (path.startsWith("/setkv")) {
+                handleSetkeyValue(out, params);
+            } else {
+                handleDefaultRoute(out);
+            }
+
+            out.close();
+            in.close();
+
+            clientSocket.close();
         }
-        outputLine = "HTTP/1.1 200 OK\r\n"
-                + "Content-Type: text/html\r\n"
-                + "\r\n"
-                + "<!DOCTYPE html>\n"
-                + "<html>\n"
-                + "<head>\n"
-                + "<meta charset=\"UTF-8\">\n"
-                + "<title>Title of the document</title>\n"
-                + "</head>\n"
-                + "<body>\n"
-                + "<h1>Mi propio mensaje</h1>\n"
-                + "</body>\n"
-                + "</html>\n";
-        out.println(outputLine);
-        out.close();
-        in.close();
-        clientSocket.close();
         serverSocket.close();
     }
 }
